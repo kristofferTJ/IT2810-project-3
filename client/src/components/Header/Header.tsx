c9f1df63e97b3a798cd81f686c70fa97bc00ca28
Isabel <isabelss@ntnu.no>
2020-10-08 13:44:34 +0200

    lagt inn searchbar paa header

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,2 +1,26 @@
 import React from 'react';
 import './Header.css';
+import 'public/user.svg';
+
+export default function Header() {
+    return(
+        <header className='header'>
+            <h1 className='title'>Foodfinder</h1>
+            <div className="searchDiv">
+                <input className='searchbar'
+                placeholder='Search for restaurants . . .'
+                >
+                </input>
+            </div>
+            <div className="options">
+                <div>
+                    <h2 className="header_option">Favorites</h2>
+                </div>
+                <div className="profilediv">
+                    <h2 className="header_option">My Site</h2>
+                    
+                </div>
+            </div>
+        </header>
+    )
+}
