7a396c4bf21a3dcd1f05632549cfd31c42dcf55f
Isabel <isabelss@ntnu.no>
2020-10-19 16:51:52 +0200

    lastet ned pakker og endringer i restaurantList

--- client/src/components/RestaurantList/RestaurantList.tsx
+++ client/src/components/RestaurantList/RestaurantList.tsx
@@ -1,43 +1,54 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './RestaurantList.css';
 import RestaurantPage from '../Restaurant page/RestaurantPage2';
-// import axios from 'axios';
-// import { render } from '@testing-library/react';
-// import { ListenOptions } from 'net';
+import axios from 'axios';
+import { render } from '@testing-library/react';
+//import { ListenOptions } from 'net';
 
 
-export default function RestaurantList() {
+// export default function RestaurantList() {
+
+  export default function RestaurantList(){
+
   return(
     <div className="container">
-      <div>
-       <input className='searchbar'
-                placeholder='Search for restaurants . . .'>
-      </input>
-    </div>
       <div className="post card">
         <div className="card-content">
-        <span className="card-title">RestaurantName</span>
+        <span className="card-title">restaurant.name</span>
         <div className="card-action">
-          
-          <p>Cuisine</p><p>Price</p>
-          <p>Region <i className="material-icons">location_on</i></p><p>City</p>
+          <p>restaurant.cuisine</p><p>Price</p>
+          <p>Region<i className="material-icons">location_on</i></p><p>City</p>
         </div>
         </div>
       </div>
     </div>
- 
-/*
+
+  /*
     state = {
     restaurants: []
   }
 
-  //skal dette skje i redux?
-    componentDidMount(){
-    axios.get('https://')
+  useEffect(() => {
+    const getRestaurants = () => {
+      axios.get('http://localhost:8000/api/restaurant')
       .then(res => {
         console.log(res)
         this.setState({
-          restaurants: res.data.slice(0,10) //viser kun de 10 første objektene
+          restaurants: JSON.parse(res)
+        })
+      })
+}
+    getRestaurants();
+}, [])
+
+
+  //skal dette skje i redux?
+    componentDidMount(){
+    axios.get('http://localhost:8000/api/restaurant')
+      .then(res => {
+        console.log(res)
+        this.setState({
+          restaurants: JSON.parse(res) 
         })
       })
   }
@@ -47,27 +58,31 @@ export default function RestaurantList() {
     const restaurantlist = restaurants.length ? (
       restaurants.map(restaurant => {
         return(
-          <div className="post card" key={restaurant.id}>
-            <div className="card-content">
+          <div className="container">
+            <div className="post card" key={restaurant.id}>
+              <div className="card-content">
               <span className="card-title">{restaurant.name}</span>
-              <p>{restaurant.cuisine}</p>
+              <div className="card-action">
+                <p>{restaurant.cuisine}</p><p>Price</p>
+                <p>Region <i className="material-icons">location_on</i></p><p>City</p>
+              </div>
+              </div>
             </div>
-          </div> 
-        )
+          </div>
       })
     ) : (
       <div className="center">No posts yet</div>
     )
   }
+  
+
     return(
       <div className="container">
-        <h4 className="center">Home</h4>
-        {restaurantList}
+        {restaurantlist}
       </div>
-    ) 
-  */
+    */
 
-  );
+  )
 };
 
 
