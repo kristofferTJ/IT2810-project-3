ef9090c21bf53da935c00b6ffbe7977dc4b79586
Kristoffer Tufte-Johnsen <kristotu@stud.ntnu.no>
2020-10-20 16:11:39 +0200

    #4 Possible to rate and make comments

--- backend/routes/api/restaurant.ts
+++ backend/routes/api/restaurant.ts
@@ -2,14 +2,19 @@ import express, { Response, NextFunction } from 'express';
 const router = express.Router();
 import { Restaurant } from '../../models/Restaurant';
 import { IUser } from '../../models/User';
+import { IRating } from '../../models/Restaurant';
+import { IComment } from '../../models/Restaurant';
 const auth = require('../../middleware/auth');
 const fs = require('fs');
 const fastcsv = require('fast-csv');
 
-let stream = fs.createReadStream(
-  'C:/Users/Bruker/Downloads/archive/three-stars-michelin-restaurants.csv'
-);
+// let stream = fs.createReadStream(
+//   'C:/Users/Bruker/Downloads/archive/three-stars-michelin-restaurants.csv'
+// );
 
+// @route   GET api/restaurant
+// @desc    Get 20 restaurants
+// @access  Pulic
 router.get('/', async (req: any, res: Response) => {
   try {
     const restaurant = await Restaurant.find().limit(20);
@@ -59,14 +64,17 @@ router.get('/', async (req: any, res: Response) => {
 //   res.send('dette gikk bra');
 // });
 
-router.put('/:restaurant_id', auth, async (req: any, res: Response) => {
+// @route   PUT api/restaurant/rate/:restaurant_id
+// @desc    Rate restaurant
+// @access  Private
+router.put('/rate/:restaurant_id', auth, async (req: any, res: Response) => {
   const { rating } = req.body;
-  let newRating: any;
 
-  newRating = {
+  const newRating = {
     user: req.user.id,
     rating,
-  };
+  } as IRating;
+
   try {
     const restaurant = await Restaurant.findById(req.params.restaurant_id);
     if (!restaurant) {
@@ -81,6 +89,43 @@ router.put('/:restaurant_id', auth, async (req: any, res: Response) => {
     }
 
     restaurant.ratings.unshift(newRating);
+
+    await restaurant.save();
+    return res.json(restaurant);
+  } catch (err) {
+    console.error(err.message);
+    if (err.kind == 'ObjectId') {
+      return res.status(400).json({ msg: 'Restaurant not found' });
+    }
+    res.status(404).json({ msg: 'Page not found' });
+  }
+});
+
+// @route   PUT api/restaurant/comment/:restaurant_id
+// @desc    comment on restaurant
+// @access  Private
+router.put('/comment/:restaurant_id', auth, async (req: any, res: Response) => {
+  const { comment } = req.body;
+
+  const newComment = {
+    user: req.user.id,
+    comment,
+  } as IComment;
+
+  try {
+    const restaurant = await Restaurant.findById(req.params.restaurant_id);
+    if (!restaurant) {
+      return res
+        .status(404)
+        .json({ msg: 'Denne restauranten er ikke i vår database' });
+    }
+    if (restaurant.comments.some((comment) => (comment.user = req.user.id))) {
+      restaurant.comments = restaurant.comments.filter((obj) => {
+        obj.user.toString() !== req.user.id.toString();
+      });
+    }
+
+    restaurant.comments.unshift(newComment);
 
     await restaurant.save();
     return res.json(restaurant);
