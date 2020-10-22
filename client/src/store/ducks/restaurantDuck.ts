9fb1ed1ffb6bdc68614bde5010678c92e5a78424
Kristoffer Tufte-Johnsen <kristotu@stud.ntnu.no>
2020-10-22 22:55:17 +0200

    bug fix

--- client/src/store/ducks/restaurantDuck.ts
+++ client/src/store/ducks/restaurantDuck.ts
@@ -1,13 +1,12 @@
-import {IRestaurant} from '../../../../backend/models/Restaurant';
+import { IRestaurant } from '../../../../backend/models/Restaurant';
 import axios from 'axios';
 
 // Types
 
 type RestaurantAction = {
-    type: string;
-    payload: IRestaurant[];
-  };
-  
+  type: string;
+  payload: IRestaurant[];
+};
 
 // Actions
 
@@ -16,38 +15,41 @@ const GET_RESTAURANTS_FAILURE = 'GET_RESTAURANTS_FAILURE';
 
 //Reducer
 
-export default function restaurantReducer(state: IRestaurant[]=[], action: RestaurantAction){
-    switch(action.type) {
-        case GET_RESTAURANTS_SUCCESS:
-            return [...action.payload];
-        case GET_RESTAURANTS_FAILURE:
-            console.log('Restaurants loading error, check if backend is connected properly');
-            return state;
-        default: 
-        return state;
-    }
+export default function restaurantReducer(
+  state: IRestaurant[] = [],
+  action: RestaurantAction
+) {
+  switch (action.type) {
+    case GET_RESTAURANTS_SUCCESS:
+      return [...action.payload];
+    case GET_RESTAURANTS_FAILURE:
+      console.log(
+        'Restaurants loading error, check if backend is connected properly'
+      );
+      return state;
+    default:
+      return state;
+  }
 }
 
-
-// Action creators 
+// Action creators
 export function fetchRestaurantsSuccess(response: any) {
-    return {
-        type: GET_RESTAURANTS_SUCCESS,
-        payload: response.data
-    };
+  return {
+    type: GET_RESTAURANTS_SUCCESS,
+    payload: response.data,
+  };
 }
 
 export function fetchRestaurantsFailure() {
-    return {
-        type: GET_RESTAURANTS_FAILURE
-    };
+  return {
+    type: GET_RESTAURANTS_FAILURE,
+  };
 }
 
 export function fetchRestaurants() {
-    return (dispatch: any) => 
+  return (dispatch: any) =>
     axios
-        .get('http://localhost:8000/api/restaurant/')
-            .then(response => dispatch(fetchRestaurantsSuccess(response)))
-            .catch(err => dispatch(fetchRestaurantsFailure));
+      .get('http://localhost:8000/api/restaurant/')
+      .then((response) => dispatch(fetchRestaurantsSuccess(response)))
+      .catch((err) => dispatch(fetchRestaurantsFailure));
 }
-
