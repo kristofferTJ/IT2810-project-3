d6efc1c0ec81e5d91068a381c70ac89692de2bcd
Isabel <isabelss@ntnu.no>
2020-10-25 16:24:50 +0100

    fikset design paa restaurantobjektene

--- client/src/components/RestaurantList/Restaurants.tsx
+++ client/src/components/RestaurantList/Restaurants.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import './RestaurantObject.css';
+import './Restaurants.css';
 import { useDispatch, useSelector } from 'react-redux';
 import {IRestaurant} from '../../../../backend/models/Restaurant';
 import { fetchRestaurants } from '../../store/ducks/restaurantDuck';
@@ -11,14 +11,22 @@ function Restaurants() {
 
 
     return(
-        <div>
-            {restaurant?.map((restaurant: IRestaurant) => (
-                <RestaurantObject
-                key={restaurant._id}
-                restaurant={restaurant}
-                ></RestaurantObject>
-            ))}
-        </div>
+        <section className="section section-icons gray lighten-4 center">
+            <div className="container">
+                <div className="restaurantlist">
+                    <div className="row">
+                    <div className="col s12 m6">
+                    {restaurant?.map((restaurant: IRestaurant) => (
+                        <RestaurantObject
+                        key={restaurant._id}
+                        restaurant={restaurant}
+                        ></RestaurantObject>
+                    ))}
+                    </div>
+                </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
